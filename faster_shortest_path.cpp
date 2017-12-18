a5c672c9489a471f26034a3960048080fd77c29c
MUHAMMED FURKAN INALCIK <e2171643@cenginek>
2017-12-18 23:33:33 +0300

    faster_shortest_path.cpp is working

--- faster_shortest_path.cpp
+++ faster_shortest_path.cpp
@@ -8,10 +8,106 @@
 #include<stdio.h>
 #include <fstream>
 #include <iostream>
+#include<bits/stdc++.h>
 
+# define INF 0x3f3f3f3f
 using namespace std;
  
 // A utility function to swap two elements
+typedef pair<int, int> iPair;
+ 
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
+{
+public:
+    int V;    // No. of vertices
+ 
+    // In a weighted graph, we need to store vertex
+    // and weight pair for every edge
+    list< pair<int, int> > *adj;
+ 
+
+    Graph(int V);  // Constructor
+ 
+    // function to add an edge to graph
+    void addEdge(int u, int v, int w);
+ 
+    // prints shortest path from s
+    void shortestPath(int s , vector<int>& dist);
+};
+ 
+// Allocates memory for adjacency list
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<iPair> [V];
+}
+ 
+void Graph::addEdge(int u, int v, int w)
+{
+    adj[u].push_back(make_pair(v, w));
+    adj[v].push_back(make_pair(u, w));
+}
+ 
+// Prints shortest paths from src to all other vertices
+void Graph::shortestPath(int src, vector<int>& dist )
+{
+    // Create a priority queue to store vertices that
+    // are being preprocessed. This is weird syntax in C++.
+    // Refer below link for details of this syntax
+    // http://geeksquiz.com/implement-min-heap-using-stl/
+    priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
+ 
+    // Create a vector for distances and initialize all
+    // distances as infinite (INF)
+    
+    //vector<int> dist(V, INF);
+ 
+    // Insert source itself in priority queue and initialize
+    // its distance as 0.
+    pq.push(make_pair(0, src));
+    dist[src] = 0;
+ 
+    /* Looping till priority queue becomes empty (or all
+      distances are not finalized) */
+    while (!pq.empty())
+    {
+        // The first vertex in pair is the minimum distance
+        // vertex, extract it from priority queue.
+        // vertex label is stored in second of pair (it
+        // has to be done this way to keep the vertices
+        // sorted distance (distance must be first item
+        // in pair)
+        int u = pq.top().second;
+        pq.pop();
+ 
+        // 'i' is used to get all adjacent vertices of a vertex
+        list< pair<int, int> >::iterator i;
+        for (i = adj[u].begin(); i != adj[u].end(); ++i)
+        {
+            // Get vertex label and weight of current adjacent
+            // of u.
+            int v = (*i).first;
+            int weight = (*i).second;
+ 
+            //  If there is shorted path to v through u.
+            if (dist[v] > dist[u] + weight)
+            {
+                // Updating distance of v
+                dist[v] = dist[u] + weight;
+
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+ 
+    // Print shortest distances stored in dist[]
+    /*printf("Vertex   Distance from Source\n");
+    for (int i = 0; i < V; ++i)
+        printf("%d \t\t %d\n", i, dist[i]);
+    */
+}
 
 int makePositive(int number){
   number = number < 0 ? 0-number : number; 
@@ -198,6 +294,9 @@ whose choice will make the biggest minimization in total distance
 
   //int myGraph[num_cities][num_cities];
 
+  Graph g(num_cities);
+
+
   int** myGraph;
   myGraph = new int*[num_cities];
   for (int i = 0; i < num_cities; ++i)
@@ -233,6 +332,8 @@ whose choice will make the biggest minimization in total distance
     } else if (count > num_dest+4)
     {
       myGraph[graph_index_1][graph_index_2] = a;
+      if(a)
+        g.addEdge(graph_index_1, graph_index_2, a );
 
       graph_index_2++;
       if (graph_index_2 == num_cities)
@@ -253,17 +354,25 @@ whose choice will make the biggest minimization in total distance
     {
       diff[i] = new int[2];
     }
-
+/*
     int* dist1;
     dist1 = new int[num_cities];
     int* dist2;
     dist2 = new int[num_cities];
+*/
+    vector<int> dist1(num_cities, INF);
+    vector<int> dist2(num_cities, INF);
+
+    g.shortestPath(wh1,dist1);
+    g.shortestPath(wh2,dist2);
+
+    list< pair<int, int> >::iterator i;
 
 
 
 
-    dijkstra(myGraph, dist1, wh1, num_cities);
-    dijkstra(myGraph, dist2, wh2, num_cities);
+    //dijkstra(myGraph, dist1, wh1, num_cities);
+    //dijkstra(myGraph, dist2, wh2, num_cities);
 
 
 
